fb54d3ab82d5227393447694458af5cb772800dd
unknown <officialedithaugustine@gmail.com>
2024-02-25 00:00:44 +0300

    Project Added

---
+++ Edith_input.py
@@ -0,0 +1,10 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> name = input("Edith")
+Edith
+>>> age = input("25")
+25
+>>> location = input("Nakuru")
+Nakuru
+>>> print("Hello World! I am Edith 25 years old and live in Nakuru")
+Hello World! I am Edith 25 years old and live in Nakuru
